174074336b769acbad377272545a328f9f55a5e0
johnatan <jmontoya12345@cesde.net>
2026-02-06 20:03:46 -0500

    creación de los elementos en product.js y las imagenes de los elementos

---
+++ src/data/products.js
@@ -0,0 +1,35 @@
+export const products = [
+  {
+    id: 1, 
+    name: 'Laptop HP Pavilion',
+    category: 'Laptops',
+    price: 4500000,
+    Image: 'src/assets/laptop.avif',
+    desccription: 'Laptop HP Pavilion con procesador Intel Core i5, 8GB de RAM y 256GB de almacenamiento SSD.'
+  },
+    {
+    id: 2, 
+    name: 'Smartphone Samsung Galaxy S21',
+    category: 'Smartphones',
+    price: 3500000,
+    Image: 'src/assets/smartphone.png',
+    desccription: 'Smartphone Samsung Galaxy S21 con pantalla AMOLED de 6.2 pulgadas, procesador Exynos 2100 y cámara de 64MP.'
+    },
+  {
+    id: 3,
+    name: 'Audífonos Sony WH-1000XM4',
+    category: 'Audio',
+    price: 1200000,
+    Image: 'src/assets/audifonos.jpg',
+    desccription: 'Audífonos Sony WH-1000XM4 con cancelación de ruido, batería de larga duración y calidad de sonido premium.'
+    },
+    {
+    id: 4,
+    name: 'Smartwatch Apple Watch Series 6',
+    category: 'Wearables',
+    price: 2500000,
+    Image: 'src/assets/smartwatch.jpg',
+    desccription: 'Smartwatch Apple Watch Series 6 con monitor de oxígeno en sangre, GPS y resistencia al agua.'
+    },
+
+]
