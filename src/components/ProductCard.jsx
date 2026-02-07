77dc5749e5557e267c4da786a97ef47534ce1643
johnatan <jmontoya12345@cesde.net>
2026-02-06 20:21:08 -0500

    creacion de product-card

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,22 @@
+function ProductCard({ name, category, price, Image, description }) {
+  return (
+    <article className="product-card">
+      <div className="product-image">
+        <img src={Image} alt={name} className="product-image" />
+      </div>
+
+      <div className="product-info">
+        <span className="product-category">{category}</span>
+        <h3 className="product-name">{name}</h3>
+        <p className="product-description">{description}</p>
+
+        <div className="product-footer">
+          <span className="product-price">${price}</span>
+          <button className="btn-like">❤️ Me gusta</button>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default ProductCard;
